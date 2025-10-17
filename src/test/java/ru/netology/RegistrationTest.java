c0f5e7af7cee015c7566e58f8b2e26c5eb4234fa
Neto test <dimmakarovdv89@mail.ru>
2025-10-17 12:17:18 +0500

    dd app-card-delivery.jar

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -8,8 +8,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -21,18 +20,17 @@ public class RegistrationTest {
         Selenide.open("http://localhost:9999");
         $("[data-test-id=city]").shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=city] input").setValue("Москва");
-        $$(".menu-item").findBy(text("Москва")).click();
 
-        $("[data-test-id=date] input").click();
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
         String meetingDate = LocalDate.now().plusDays(3)
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] input").setValue(meetingDate);
-
         $("[data-test-id=name] input").setValue("Иванов Иван Иваныч");
         $("[data-test-id=phone] input").setValue("+79191468952");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(20))
+                .shouldHave(exactText("Встреча успешно забронирована на " + meetingDate));
     }
 }
